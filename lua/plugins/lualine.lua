fb74dd3f8a82c8d2d406555d251c94f3c0a3a33b
Prajval Badiger <er.prajvalbadiger@gmail.com>
2024-03-23 14:53:08 +0530

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,35 @@
 -- Fancier statusline
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		local colorscheme = require("helpers.colorscheme")
-		local lualine_theme = colorscheme == "default" and "auto" or colorscheme
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = lualine_theme,
-				component_separators = "|",
-				section_separators = "",
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        { 'nvim-tree/nvim-web-devicons', lazy = true },
+    },
+    config = function()
+        local colorscheme = require("helpers.colorscheme")
+        local lualine_theme = colorscheme == "default" and "auto" or colorscheme
+        require("lualine").setup({
+            options = {
+                icons_enabled = true,
+                theme = lualine_theme,
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                always_divide_middle = true,
+                globalstatus = true,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'arduino_status' },
+                    lualine_y = { 'filetype' },
+                    lualine_z = { 'progress' }
+                },
+                extensions = { 'quickfix', 'fugitive' }
+            },
+        })
+    end,
 }
