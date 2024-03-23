25071e2544a045e9a26cc9d040c741fdb075f69d
Prajval Badiger <er.prajvalbadiger@gmail.com>
2024-03-24 00:45:58 +0530

    vim tmux navigation

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,12 +2,29 @@
 return {
     -- Comment with haste
     {
-	"numToStr/Comment.nvim",
-	event = "BufReadPre",
-    	opts = {},
+        "numToStr/Comment.nvim",
+        event = "BufReadPre",
+        opts = {},
     },
     {
-    	"tpope/vim-surround", -- Surround stuff with the ys-, cs-, ds- commands
-    	event = "BufReadPre",
+        "tpope/vim-surround", -- Surround stuff with the ys-, cs-, ds- commands
+        event = "BufReadPre",
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
     },
 }
