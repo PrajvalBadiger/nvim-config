0de544821b809c056d8c34c8a771c59857257234
Prajval Badiger <er.prajvalbadiger@gmail.com>
2024-03-25 15:13:13 +0530

    opts and keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,56 +1,62 @@
 local map = require("helpers.keys").map
 
--- Blazingly fast way out of insert mode
-map("i", "jk", "<esc>")
-
--- Quick access to some common actions
-map("n", "<leader>fw", "<cmd>w<cr>", "Write")
-map("n", "<leader>fa", "<cmd>wa<cr>", "Write all")
-map("n", "<leader>qq", "<cmd>q<cr>", "Quit")
-map("n", "<leader>qa", "<cmd>qa!<cr>", "Quit all")
-map("n", "<leader>dw", "<cmd>close<cr>", "Window")
-
--- Diagnostic keymaps
-map('n', 'gx', vim.diagnostic.open_float, "Show diagnostics under cursor")
-
 -- Easier access to beginning and end of lines
 map("n", "<M-h>", "^", "Go to beginning of line")
 map("n", "<M-l>", "$", "Go to end of line")
-
--- Move with shift-arrows
-map("n", "<S-Left>", "<C-w><S-h>", "Move window to the left")
-map("n", "<S-Down>", "<C-w><S-j>", "Move window down")
-map("n", "<S-Up>", "<C-w><S-k>", "Move window up")
-map("n", "<S-Right>", "<C-w><S-l>", "Move window to the right")
-
--- Resize with arrows
-map("n", "<C-Up>", ":resize +2<CR>")
-map("n", "<C-Down>", ":resize -2<CR>")
-map("n", "<C-Left>", ":vertical resize +2<CR>")
-map("n", "<C-Right>", ":vertical resize -2<CR>")
-
--- Deleting buffers
-local buffers = require("helpers.buffers")
-map("n", "<leader>db", buffers.delete_this, "Current buffer")
-map("n", "<leader>do", buffers.delete_others, "Other buffers")
-map("n", "<leader>da", buffers.delete_all, "All buffers")
-
--- Navigate buffers
-map("n", "<S-l>", ":bnext<CR>")
-map("n", "<S-h>", ":bprevious<CR>")
 
 -- Stay in indent mode
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- Switch between light and dark modes
-map("n", "<leader>ut", function()
-	if vim.o.background == "dark" then
-		vim.o.background = "light"
-	else
-		vim.o.background = "dark"
-	end
-end, "Toggle between light and dark themes")
-
 -- Clear after search
 map("n", "<leader>ur", "<cmd>nohl<cr>", "Clear highlights")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Project View' })
+vim.keymap.set("n", "<leader>pe", vim.cmd.Lex, { desc = 'Project Explore' })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste from clipboard' })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'Yank to clipboard' })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Yank a line to clipboard' })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = 'Format Buffer' })
+
+vim.keymap.set("n", "<leader>k", "<cmd>cnext<CR>", { desc = 'Quickfix: next' })
+vim.keymap.set("n", "<leader>j", "<cmd>cprev<CR>", { desc = 'Quickfix: prev' })
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Substitute' })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>",
+    { silent = true, desc = 'Provide Execute permission to current file' })
+
+vim.keymap.set("n", "<leader>k", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>cprev<CR>zz")
+
+vim.keymap.set("n", ";", ":")
+
+vim.keymap.set("i", "jj", "<ESC>")
+vim.keymap.set("i", "JJ", "<ESC>")
+
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
+vim.keymap.set("n", "<", "<gv", { noremap = true })
+vim.keymap.set("n", ">", ">gv", { noremap = true })
+
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,16 +1,45 @@
 local opts = {
-	shiftwidth = 4,
-	tabstop = 4,
-	expandtab = true,
-	wrap = false,
-	termguicolors = true,
-	number = true,
-	relativenumber = true,
+    nu = true,
+    rnu = true,
+
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 4,
+    expandtab = true,
+    smartindent = true,
+
+    wrap = false,
+
+    swapfile = false,
+    backup = false,
+    undofile = true,
+    undodir = os.getenv("HOME") .. "/.vim/undodir",
+
+    hlsearch = false,
+    incsearch = true,
+
+    termguicolors = true,
+    scrolloff = 8,
+
+    signcolumn = "auto",
+
+    updatetime = 50,
+
+    splitright = true,
+    splitbelow = true,
+    cursorline = false,
+    laststatus = 3,
+
+    list = false,
+    mouse = "a",
+    autoread = true,
+    --  guicursor = ""
+    --  colorcolumn = "80",
 }
 
 -- Set options from table
 for opt, val in pairs(opts) do
-	vim.o[opt] = val
+    vim.o[opt] = val
 end
 
 -- Set other options
